b2a1f367177153c5c19cc774829132c3a5bfe7f2
fedannie <anyutka-fedorova@mail.ru>
2016-11-20 17:15:02 +0300

    Popup window
    Added Popup wondow in dictionary, the one to filter the themes

--- app/src/main/java/project/fedorova/polyglotte/DictActivity.java
+++ app/src/main/java/project/fedorova/polyglotte/DictActivity.java
@@ -1,15 +1,21 @@
 package project.fedorova.polyglotte;
 
 import android.app.Activity;
+import android.view.ViewGroup.LayoutParams;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class DictActivity extends Activity implements View.OnClickListener {
+    private Button filterBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,8 @@ public class DictActivity extends Activity implements View.OnClickListener {
 
         FloatingActionButton addWordBtn = (FloatingActionButton) findViewById(R.id.addWordFAB);
         addWordBtn.setOnClickListener(this);
+        filterBtn = (Button) findViewById(R.id.filterButton);
+        filterBtn.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
@@ -25,8 +33,25 @@ public class DictActivity extends Activity implements View.OnClickListener {
                 Intent intentAWA = new Intent(this, AddWordActivity.class);
                 startActivity(intentAWA);
                 break;
+            case (R.id.filterButton):
+                showPopup();
+                break;
             default:
                 break;
         }
+    }
+
+    private PopupWindow pw;
+    private void showPopup() {
+        LayoutInflater layoutInflater
+                = (LayoutInflater)getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.popupfilterthemes, null);
+        final PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT,
+                false);
+        popupWindow.showAsDropDown(filterBtn, 50, -30);
     }
 }
---
+++ app/src/main/java/project/fedorova/polyglotte/PopUpFilterThemes.java
@@ -0,0 +1,19 @@
+package project.fedorova.polyglotte;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class PopUpFilterThemes extends Activity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popupfilterthemes);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
