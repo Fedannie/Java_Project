e463c8c3665a7d38048ff010fca16906c1a24d78
fedannie <anyutka-fedorova@mail.ru>
2016-11-26 22:41:21 +0300

    Native language changer added
    Native language changer added in preference screen

--- app/src/main/java/project/fedorova/polyglotte/MainMenuActivity.java
+++ app/src/main/java/project/fedorova/polyglotte/MainMenuActivity.java
@@ -2,11 +2,13 @@ package project.fedorova.polyglotte;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainMenuActivity extends Activity implements View.OnClickListener {
+    SharedPreferences sPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button btnExercise;
@@ -14,6 +16,7 @@ public class MainMenuActivity extends Activity implements View.OnClickListener {
         Button btnDict;
         Button btnPhrase;
         Button btnTrans;
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
--- app/src/main/java/project/fedorova/polyglotte/PrefActivity.java
+++ app/src/main/java/project/fedorova/polyglotte/PrefActivity.java
@@ -1,13 +1,36 @@
 package project.fedorova.polyglotte;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 public class PrefActivity extends PreferenceActivity {
-
+    SharedPreferences sPref;
+    ListPreference nativeLanguage;
+    private final static String LANGUAGE = "native language";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        nativeLanguage = (ListPreference) findPreference("nativeLanguage");
+        nativeLanguage.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                changeNativeLanguage();
+                return true;
+            }
+        });
+    }
+
+    private void changeNativeLanguage() {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(LANGUAGE, nativeLanguage.getValue());
+        ed.commit();
+        Toast.makeText(this, "Your native language changed", Toast.LENGTH_SHORT).show();
     }
 }
