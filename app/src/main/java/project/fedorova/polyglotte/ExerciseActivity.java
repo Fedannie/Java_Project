45a0f7c070dc8def896cde95d784623ed3b16727
fedannie <anyutka-fedorova@mail.ru>
2016-12-05 23:23:22 +0300

    ExerciseActivity was changed
    ExerciseActivity was changed

--- app/src/main/java/project/fedorova/polyglotte/ExerciseActivity.java
+++ app/src/main/java/project/fedorova/polyglotte/ExerciseActivity.java
@@ -3,17 +3,47 @@ package project.fedorova.polyglotte;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import project.fedorova.polyglotte.layouts.CenterLockHorizontalScrollview;
+import project.fedorova.polyglotte.layouts.ExerciseListAdapter;
+
 
 public class ExerciseActivity extends Activity implements View.OnClickListener{
 
+    private Button wholeBtn;
+    private Button prevExerciseBtn;
+    private Button nextExerciseBtn;
+    private CenterLockHorizontalScrollview exerciseScrollView;
+    private int currIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercise);
+        wholeBtn = (Button) findViewById(R.id.startExercise);
+        wholeBtn.setOnClickListener(this);
+        prevExerciseBtn = (Button) findViewById(R.id.prev);
+        wholeBtn.setOnClickListener(this);
+        nextExerciseBtn = (Button) findViewById(R.id.next);
+        wholeBtn.setOnClickListener(this);
+        exerciseScrollView = (CenterLockHorizontalScrollview) findViewById(R.id.scrollView);
+        ExerciseListAdapter exerciseLA = new ExerciseListAdapter(this);
+        exerciseScrollView.setAdapter(this, exerciseLA);
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.prev) {
+            if (currIndex != 0) {
+                currIndex--;
+                exerciseScrollView.setCenter(currIndex);
+            }
+        } else if (v.getId() == R.id.next) {
 
+            if (currIndex < ExerciseListAdapter.SIZE) {
+                exerciseScrollView.setCenter(currIndex);
+                currIndex++;
+            }
+        }
     }
 }
---
+++ app/src/main/java/project/fedorova/polyglotte/excercise/Exercise.java
@@ -0,0 +1,21 @@
+package project.fedorova.polyglotte.excercise;
+
+import java.util.SplittableRandom;
+
+public class Exercise {
+    private String title;
+    //private String image;
+
+    public Exercise(String newTitle/*, String newImage*/) {
+        title = newTitle;
+        //image = newImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    /*public String getImage() {
+        return image;
+    }*/
+}
