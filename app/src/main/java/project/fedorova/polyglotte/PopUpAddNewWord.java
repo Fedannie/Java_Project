8802958471c1dd55f56ea0a332b937eaf628c652
fedannie <anyutka-fedorova@mail.ru>
2016-12-12 21:45:13 +0300

    Added errors to TextInputLayout
    Added errors to TextInputLayout in popUp addNewWord activity

--- app/src/main/java/project/fedorova/polyglotte/PopUpAddNewWord.java
+++ app/src/main/java/project/fedorova/polyglotte/PopUpAddNewWord.java
@@ -2,15 +2,17 @@ package project.fedorova.polyglotte;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.RequiresPermission;
+import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.lang.ref.WeakReference;
 import java.util.UUID;
 
 import project.fedorova.polyglotte.data.DataBase.DBConnector;
@@ -18,12 +20,15 @@ import project.fedorova.polyglotte.data.ReadWriteManager;
 import project.fedorova.polyglotte.data.Word;
 
 public class PopUpAddNewWord extends Activity implements View.OnClickListener{
+    private static final String ONLY_COMMAS = "Sorry, only commas to divide words";
+    private static final String ONLY_LETTERS = "Sorry, only letters and numbers.";
     private Button addThemeBtn;
-    private EditText wordET;
-    private EditText mainTranslationET;
-    private EditText extraTranslationsET;
     private Button saveWordBtn;
     private DBConnector wordBase;
+    private TextInputLayout wordTIL;
+    private TextInputLayout mainTranslationTIL;
+    private TextInputLayout translationTIL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +42,10 @@ public class PopUpAddNewWord extends Activity implements View.OnClickListener{
         saveWordBtn = (Button) findViewById(R.id.saveWord);
         saveWordBtn.setOnClickListener(this);
 
-        wordET = (EditText) findViewById(R.id.wordET);
-        wordET.addTextChangedListener(new TextWatcher(){
+        wordTIL = (TextInputLayout) findViewById(R.id.wordinput);
+        wordTIL.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public void afterTextChanged(Editable s) {
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
             }
 
             @Override
@@ -48,14 +53,20 @@ public class PopUpAddNewWord extends Activity implements View.OnClickListener{
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void afterTextChanged(Editable s) {
+                if (containsPunct(s.toString())) {
+                    wordTIL.setErrorEnabled(true);
+                    wordTIL.setError(ONLY_LETTERS);
+                } else {
+                    wordTIL.setErrorEnabled(false);
+                }
             }
         });
 
-        mainTranslationET = (EditText) findViewById(R.id.translationET);
-        mainTranslationET.addTextChangedListener(new TextWatcher(){
+        mainTranslationTIL = (TextInputLayout) findViewById(R.id.translationinput);
+        mainTranslationTIL.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public void afterTextChanged(Editable s) {
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
             }
 
             @Override
@@ -63,15 +74,20 @@ public class PopUpAddNewWord extends Activity implements View.OnClickListener{
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void afterTextChanged(Editable s) {
+                if (containsPunct(mainTranslationTIL.getEditText().getText().toString())) {
+                    mainTranslationTIL.setErrorEnabled(true);
+                    mainTranslationTIL.setError(ONLY_LETTERS);
+                } else {  //TODO Translate word and check the translation
+                    mainTranslationTIL.setErrorEnabled(false);
+                }
             }
         });
 
-        extraTranslationsET = (EditText) findViewById(R.id.extratranslationsET);
-        extraTranslationsET.addTextChangedListener(new TextWatcher(){
+        translationTIL = (TextInputLayout) findViewById(R.id.extratranslationsinput);
+        translationTIL.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
-            public void afterTextChanged(Editable s) {
-
+            public void onTextChanged(CharSequence text, int start, int count, int after) {
             }
 
             @Override
@@ -79,7 +95,13 @@ public class PopUpAddNewWord extends Activity implements View.OnClickListener{
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void afterTextChanged(Editable s) {
+                if (containsPunctExceptComma(translationTIL.getEditText().getText().toString())) {
+                    translationTIL.setErrorEnabled(true);
+                    translationTIL.setError(ONLY_COMMAS);
+                } else {
+                    translationTIL.setErrorEnabled(false);
+                }
             }
         });
 
@@ -92,14 +114,24 @@ public class PopUpAddNewWord extends Activity implements View.OnClickListener{
                 break;
             case (R.id.saveWord):
                 ReadWriteManager readWriteManager = ReadWriteManager.getInstance();
-                wordBase.insertWord(new Word(UUID.randomUUID(), wordET.getText().toString(),
-                        mainTranslationET.getText().toString(),
-                        readWriteManager.convertStringToSet(extraTranslationsET.getText().toString()),
+                wordBase.insertWord(new Word(UUID.randomUUID(), wordTIL.getEditText().getText().toString(),
+                        mainTranslationTIL.getEditText().getText().toString(),
+                        readWriteManager.convertStringToSet(translationTIL.getEditText().getText().toString()),
                         null));
-                super.onDestroy();
+                finish();
                 break;
             default:
                 break;
         }
+    }
+
+    private boolean containsPunctExceptComma (String text) {
+        String check = text.replace(",", "").replace(" ", "");
+        return check.equals(check.replaceAll("[\\p{Punct}]", ""));
+    }
+
+    private boolean containsPunct(String text) {
+        String check = text.replace(" ", "");
+        return check.equals(check.replaceAll("[\\p{Punct}]", ""));
     }
 }
